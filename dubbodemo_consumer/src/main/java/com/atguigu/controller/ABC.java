fc0b6d2591edcb577c4dbf4e128d227fcb79c10d
谢晨昊 <965587781@qq.com>
2021-02-02 16:26:40 +0800

    练习上传项目---IDEA2-master-练习push

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -10,5 +10,7 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("123456798master");
         System.out.println("123456798DEV2");
+        System.out.println("11111111111");
+        System.out.println("22222222222");
     }
 }
