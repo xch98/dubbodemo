23c4b9e286fa152912bfc3f341be4326e1cd2f28
谢晨昊 <965587781@qq.com>
2021-02-02 17:14:23 +0800

    123456

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -8,6 +8,6 @@ package com.atguigu.controller;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("1234657dev2");
+        System.out.println("12346572");
     }
 }
