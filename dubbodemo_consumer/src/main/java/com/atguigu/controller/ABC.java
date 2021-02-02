4d764a12ff45d1eb89c56f51ddaebfd84264095b
谢晨昊 <965587781@qq.com>
2021-02-02 16:40:13 +0800

    练习上传项目---测试删除

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -9,9 +9,5 @@ package com.atguigu.controller;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("123456798master");
-        System.out.println("123456798DEV2");
-        System.out.println("11111111111");
-        System.out.println("22222222222");
-        System.out.println("333333333333");
     }
 }
