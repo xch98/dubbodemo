12824a0bb0c69dc22eb754108f1498b8ae0d87b8
谢晨昊 <965587781@qq.com>
2021-02-02 15:49:13 +0800

    练习上传项目---IDEA2

---
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -0,0 +1,13 @@
+package com.atguigu.controller;
+
+/**
+ * @author: xch
+ * @Date: 2021/2/2 15:02
+ * Version 1.0
+ */
+
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("123456798");
+    }
+}
